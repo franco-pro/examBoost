9ae75a4032917df3a8745e091bb4c81d71507a70
Franz Deussom <franzdeussom111@gmail.com>
2025-10-21 17:41:14 +0200

    redux init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,17 @@ import { Stack } from 'expo-router';
 
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function RootLayout(){
     return(
+      <Provider store={store}>
         <GluestackUIProvider>
                 <Stack screenOptions={{ headerShown: false }}>
                     <Stack.Screen name="(tabs)" />
                 </Stack>
         </GluestackUIProvider>
+      </Provider>
     )
 }
