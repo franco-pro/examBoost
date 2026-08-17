2a445ae8cf87e2b5dec3014420fa1f3204690032
Franz Deussom <franzdeussom111@gmail.com>
2026-08-17 13:32:34 +0100

    notification type update, ui: merge with FD: area view on maj screen

--- app/api/apiClient.ts
+++ app/api/apiClient.ts
@@ -5,10 +5,10 @@ import { updateTokens } from "../hooks/redux/users/users.slice";
 import { useDispatch } from "react-redux";
 import { store } from "../hooks/redux/store";
 
-export const BASE_URL = "http://172.20.10.4";
-export const socketUrl = "http://172.20.10.4:3000";
+export const BASE_URL = "https://www.examboost.org/api";
+export const socketUrl = "https://www.examboost.org";
 export const apiClient = axios.create({
-  baseURL:`${BASE_URL}:3000`,
+  baseURL:`${BASE_URL}`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
@@ -34,6 +34,19 @@ apiClient.interceptors.request.use(async (config) => {
   }
   return config;
 });
+//Gerer la file d'attente de token
+let isRefreshing = false;
+let failedQueue:any = [];
+const processQueue = (error:any, token = null) => {
+  failedQueue.forEach((prom:any) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
 //refresh le acceess token si expiré avec le refresh token
 apiClient.interceptors.response.use(
   (response) => response,
@@ -50,41 +63,61 @@ apiClient.interceptors.response.use(
 
     //si token a une status 401 donc si le accessToken a expire , utiliser le refresh token pour refresh le accessToken
     if (error.response?.status === 401 && !originalRequest._retry) {
+      // CAS 1 : Si un rafraîchissement est DÉJÀ en cours pour une autre requête
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers["Authorization"] = `Bearer ${token}`;
+            return apiClient.request(originalRequest);
+          })
+          .catch((err) => Promise.reject(err));
+      }
       originalRequest._retry = true;
+      isRefreshing = true;
       console.log("STATUS ERROR 👉", error.response?.status);
       try {
         console.log("enter refresh fonction");
         const refresh_Token = await getItem("refreshToken");
         console.log("refresh Token ApiClient:", refresh_Token);
         if (!refresh_Token) throw new Error("No refresh token found");
-        const res = await apiClient.post("/auth/refresh", { refresh_Token });
-
+        const res = await axios.post(`${BASE_URL}/auth/refresh`, {
+          refresh_Token,
+        });
+const { accessToken, refreshToken } = res.data;
         // stocke les nouveaux tokens (sans dépendre du Redux store pour éviter les cycles)
-        await setItem("accessToken", res.data.accessToken);
-        await setItem("refreshToken", res.data.refreshToken);
+        await setItem("accessToken", accessToken);
+        await setItem("refreshToken", refreshToken);
 
         store.dispatch(
           updateTokens({
-            accessToken: res.data.accessToken,
-            refreshToken: res.data.refreshToken,
+            accessToken: accessToken,
+            refreshToken: refreshToken,
           }),
         );
 
+        // Libère toutes les requêtes qui attendaient patiemment dans la file
+        processQueue(null, accessToken);
+        isRefreshing = false;
+
         console.log(
           "les keys dans refresh function :",
-          res.data.accessToken,
-          res.data.refreshToken,
+          accessToken,
+          refreshToken,
           "res:",
           res,
         );
 
         //réessaye la requete avec le nouveau accessToken
         originalRequest.headers["Authorization"] =
-          `Bearer ${res.data.accessToken}`;
+          `Bearer ${accessToken}`;
 
         return apiClient.request(originalRequest);
-      } catch (error) {
-        console.log("refresh token invalide:", error);
+      } catch (refreshError) {
+        console.log("refresh token invalide:", refreshError);
+         processQueue(refreshError, null);
+         isRefreshing = false;
         // await AsyncStorage.multiRemove(["accessToken", "refreshToken"]);
         //se deconnecter
         // store.dispatch(logout());
